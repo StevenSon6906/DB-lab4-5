ddfaf085d93e639281128943b755cdcbc7d49b8b
Arsen <arsen.shulak@gmail.com>
2025-10-03 21:15:58 +0300

    blah

--- my_project/__init__.py
+++ my_project/__init__.py
@@ -4,7 +4,7 @@ import secrets
 from typing import Dict, Any
 
 from flask import Flask
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, fields
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import database_exists, create_database
 
@@ -31,16 +31,16 @@ def create_app(app_config: Dict[str, Any], additional_config: Dict[str, Any]) ->
     _init_db(app)
     register_routes(app)
     _init_swagger(app)
-    _init_trigger(app)
-    _init_function(app)
+    #_init_trigger(app)
+    #_init_function(app)
 
     #_init_programs(app)
     # _init_procedures(app)
-    add_program_log(app, visitor_id=2, exercise_id=1, log_date="2024-10-25", unit="reps")
+    #add_program_log(app, visitor_id=2, exercise_id=1, log_date="2024-10-25", unit="reps")
 
     # _do_cursor_task(app)
 
-    _init_program_exercise(app, 1,2, 10)
+    # _init_program_exercise(app, 1, 2, 10)  # disabled: caused FK error when seed rows are missing
     return app
 
 def _do_cursor_task(app:Flask):
@@ -214,26 +214,279 @@ def _init_trigger(app: Flask) -> None:
 
 
 def _init_swagger(app: Flask) -> None:
-    # A-lia Swagger
-    restx_api = Api(app, title='Pavelchak test backend',
-                    description='A simple backend')  # https://flask-restx.readthedocs.io/
+    restx_api = Api(app, title='Fitness Center API',
+                    description='API for managing fitness center operations',
+                    version='1.0',
+                    doc='/swagger')
 
-    @restx_api.route('/number/<string:todo_id>')
-    class TodoSimple(Resource):
-        @staticmethod
-        def get(todo_id):
-            return todos, 202
+    # Models
+    exercise_model = restx_api.model('Exercise', {
+        'id': fields.Integer(readonly=True, description='Exercise identifier'),
+        'name': fields.String(required=True, description='Exercise name'),
+        'description': fields.String(description='Exercise description'),
+        'unit': fields.String(description='Exercise measurement unit (e.g., reps, kg, minutes)')
+    })
 
-        @staticmethod
-        def put(todo_id):
-            todos[todo_id] = todo_id
-            return todos, HTTPStatus.CREATED
+    program_model = restx_api.model('Program', {
+        'id': fields.Integer(readonly=True, description='Program identifier'),
+        'name': fields.String(required=True, description='Program name'),
+        'description': fields.String(required=True, description='Program description')
+    })
 
-    @app.route("/hi")
-    def hello_world():
-        return todos, HTTPStatus.OK
+    gender_model = restx_api.model('Gender', {
+        'id': fields.Integer(readonly=True, description='Gender identifier'),
+        'name': fields.String(required=True, description='Gender name')
+    })
 
+    trainer_model = restx_api.model('Trainer', {
+        'id': fields.Integer(readonly=True, description='Trainer identifier'),
+        'name': fields.String(required=True, description='Trainer name'),
+        'gender': fields.Integer(required=True, description='Gender ID'),
+        'phone': fields.String(required=True, description='Phone number')
+    })
 
+    visitor_model = restx_api.model('Visitor', {
+        'id': fields.Integer(readonly=True, description='Visitor identifier'),
+        'name': fields.String(required=True, description='Visitor name'),
+        'gender': fields.Integer(required=True, description='Gender ID'),
+        'phone': fields.String(required=True, description='Phone number')
+    })
+
+    program_exercise_model = restx_api.model('ProgramExercise', {
+        'program_id': fields.Integer(required=True, description='Program ID'),
+        'exercise_id': fields.Integer(required=True, description='Exercise ID'),
+        'target_value': fields.Integer(required=True, description='Target value for the exercise')
+    })
+
+    # Namespaces
+    exercise_ns = restx_api.namespace('exercises', description='Exercise operations')
+    program_ns = restx_api.namespace('programs', description='Program operations')
+    gender_ns = restx_api.namespace('genders', description='Gender operations')
+    trainer_ns = restx_api.namespace('trainers', description='Trainer operations')
+    visitor_ns = restx_api.namespace('visitors', description='Visitor operations')
+    program_exercise_ns = restx_api.namespace('program-exercises', description='Program Exercise operations')
+
+    # Exercise endpoints
+    @exercise_ns.route('/')
+    class ExerciseList(Resource):
+        @exercise_ns.doc('list_exercises')
+        @exercise_ns.marshal_list_with(exercise_model)
+        def get(self):
+            """List all exercises"""
+            return []
+
+        @exercise_ns.doc('create_exercise')
+        @exercise_ns.expect(exercise_model)
+        @exercise_ns.marshal_with(exercise_model, code=201)
+        def post(self):
+            """Create a new exercise"""
+            return {}
+
+    @exercise_ns.route('/<int:id>')
+    class Exercise(Resource):
+        @exercise_ns.doc('get_exercise')
+        @exercise_ns.marshal_with(exercise_model)
+        def get(self, id):
+            """Get an exercise by ID"""
+            return {}
+
+        @exercise_ns.doc('update_exercise')
+        @exercise_ns.expect(exercise_model)
+        @exercise_ns.marshal_with(exercise_model)
+        def put(self, id):
+            """Update an exercise"""
+            return {}
+
+        @exercise_ns.doc('delete_exercise')
+        @exercise_ns.response(204, 'Exercise deleted')
+        def delete(self, id):
+            """Delete an exercise"""
+            return '', 204
+
+    # Program endpoints
+    @program_ns.route('/')
+    class ProgramList(Resource):
+        @program_ns.doc('list_programs')
+        @program_ns.marshal_list_with(program_model)
+        def get(self):
+            """List all programs"""
+            return []
+
+        @program_ns.doc('create_program')
+        @program_ns.expect(program_model)
+        @program_ns.marshal_with(program_model, code=201)
+        def post(self):
+            """Create a new program"""
+            return {}
+
+    @program_ns.route('/<int:id>')
+    class Program(Resource):
+        @program_ns.doc('get_program')
+        @program_ns.marshal_with(program_model)
+        def get(self, id):
+            """Get a program by ID"""
+            return {}
+
+        @program_ns.doc('update_program')
+        @program_ns.expect(program_model)
+        @program_ns.marshal_with(program_model)
+        def put(self, id):
+            """Update a program"""
+            return {}
+
+        @program_ns.doc('delete_program')
+        @program_ns.response(204, 'Program deleted')
+        def delete(self, id):
+            """Delete a program"""
+            return '', 204
+
+    # Gender endpoints
+    @gender_ns.route('/')
+    class GenderList(Resource):
+        @gender_ns.doc('list_genders')
+        @gender_ns.marshal_list_with(gender_model)
+        def get(self):
+            """List all genders"""
+            return []
+
+        @gender_ns.doc('create_gender')
+        @gender_ns.expect(gender_model)
+        @gender_ns.marshal_with(gender_model, code=201)
+        def post(self):
+            """Create a new gender"""
+            return {}
+
+    @gender_ns.route('/<int:id>')
+    class Gender(Resource):
+        @gender_ns.doc('get_gender')
+        @gender_ns.marshal_with(gender_model)
+        def get(self, id):
+            """Get a gender by ID"""
+            return {}
+
+        @gender_ns.doc('update_gender')
+        @gender_ns.expect(gender_model)
+        @gender_ns.marshal_with(gender_model)
+        def put(self, id):
+            """Update a gender"""
+            return {}
+
+        @gender_ns.doc('delete_gender')
+        @gender_ns.response(204, 'Gender deleted')
+        def delete(self, id):
+            """Delete a gender"""
+            return '', 204
+
+    # Trainer endpoints
+    @trainer_ns.route('/')
+    class TrainerList(Resource):
+        @trainer_ns.doc('list_trainers')
+        @trainer_ns.marshal_list_with(trainer_model)
+        def get(self):
+            """List all trainers"""
+            return []
+
+        @trainer_ns.doc('create_trainer')
+        @trainer_ns.expect(trainer_model)
+        @trainer_ns.marshal_with(trainer_model, code=201)
+        def post(self):
+            """Create a new trainer"""
+            return {}
+
+    @trainer_ns.route('/<int:id>')
+    class Trainer(Resource):
+        @trainer_ns.doc('get_trainer')
+        @trainer_ns.marshal_with(trainer_model)
+        def get(self, id):
+            """Get a trainer by ID"""
+            return {}
+
+        @trainer_ns.doc('update_trainer')
+        @trainer_ns.expect(trainer_model)
+        @trainer_ns.marshal_with(trainer_model)
+        def put(self, id):
+            """Update a trainer"""
+            return {}
+
+        @trainer_ns.doc('delete_trainer')
+        @trainer_ns.response(204, 'Trainer deleted')
+        def delete(self, id):
+            """Delete a trainer"""
+            return '', 204
+
+    # Visitor endpoints
+    @visitor_ns.route('/')
+    class VisitorList(Resource):
+        @visitor_ns.doc('list_visitors')
+        @visitor_ns.marshal_list_with(visitor_model)
+        def get(self):
+            """List all visitors"""
+            return []
+
+        @visitor_ns.doc('create_visitor')
+        @visitor_ns.expect(visitor_model)
+        @visitor_ns.marshal_with(visitor_model, code=201)
+        def post(self):
+            """Create a new visitor"""
+            return {}
+
+    @visitor_ns.route('/<int:id>')
+    class Visitor(Resource):
+        @visitor_ns.doc('get_visitor')
+        @visitor_ns.marshal_with(visitor_model)
+        def get(self, id):
+            """Get a visitor by ID"""
+            return {}
+
+        @visitor_ns.doc('update_visitor')
+        @visitor_ns.expect(visitor_model)
+        @visitor_ns.marshal_with(visitor_model)
+        def put(self, id):
+            """Update a visitor"""
+            return {}
+
+        @visitor_ns.doc('delete_visitor')
+        @visitor_ns.response(204, 'Visitor deleted')
+        def delete(self, id):
+            """Delete a visitor"""
+            return '', 204
+
+    # Program Exercise endpoints
+    @program_exercise_ns.route('/')
+    class ProgramExerciseList(Resource):
+        @program_exercise_ns.doc('list_program_exercises')
+        @program_exercise_ns.marshal_list_with(program_exercise_model)
+        def get(self):
+            """List all program exercises"""
+            return []
+
+        @program_exercise_ns.doc('create_program_exercise')
+        @program_exercise_ns.expect(program_exercise_model)
+        @program_exercise_ns.marshal_with(program_exercise_model, code=201)
+        def post(self):
+            """Create a new program exercise"""
+            return {}
+
+    @program_exercise_ns.route('/<int:program_id>/<int:exercise_id>')
+    class ProgramExercise(Resource):
+        @program_exercise_ns.doc('get_program_exercise')
+        @program_exercise_ns.marshal_with(program_exercise_model)
+        def get(self, program_id, exercise_id):
+            """Get a program exercise by Program ID and Exercise ID"""
+            return {}
+
+        @program_exercise_ns.doc('update_program_exercise')
+        @program_exercise_ns.expect(program_exercise_model)
+        @program_exercise_ns.marshal_with(program_exercise_model)
+        def put(self, program_id, exercise_id):
+            """Update a program exercise"""
+            return {}
+
+        @program_exercise_ns.doc('delete_program_exercise')
+        @program_exercise_ns.response(204, 'Program exercise deleted')
+        def delete(self, program_id, exercise_id):
+            """Delete a program exercise"""
+            return '', 204
 def _init_db(app: Flask) -> None:
     """
     Initializes DB with SQLAlchemy
